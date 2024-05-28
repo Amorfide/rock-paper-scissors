d5f64a515089d5c6e9aa55b9b9d9b106d02b3cb1
amorfide <kiasukin@gmail.com>
2024-05-28 22:15:06 +0100

    allowed choices to become global, created round functionality

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,4 @@
 function getComputerChoice() {
-    let computerChoice;
     //return a random choice of rock, paper scissors
     const computerRoll = Math.floor(Math.random()*3); //start by using some random function
     switch(computerRoll){
@@ -17,15 +16,32 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const humanChoice = prompt("Please choose Rock (r), Paper (p) or Scissors (s)");
+    humanChoice = prompt("Please choose Rock (r), Paper (p) or Scissors (s)");
     
-    if(humanChoice === "r"){
+    if(humanChoice.toLowerCase() === "r"){
         console.log("You chose: Rock");
+        return humanChoice;
     }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === "r" && computerChoice === "Rock"){
+        console.log("This round was a draw")
+    }else if(humanChoice === "r" && computerChoice === "Paper"){
+        humanScore += 1;
+        console.log("You won!")
+    }
+
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+let computerChoice = "";
+let humanChoice = "";
+
 getHumanChoice();
+console.log(humanChoice);
 getComputerChoice();
+console.log(computerChoice)
+playRound(humanChoice, computerChoice);
