5b7461d99ef629d821a7127aed9a965698e83580
amorfide <kiasukin@gmail.com>
2024-05-28 21:42:48 +0100

    added human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,28 @@
 function getComputerChoice() {
+    let computerChoice;
     //return a random choice of rock, paper scissors
-    const computerChoice = Math.floor(Math.random()*3); //start by using some random function
-    console.log(computerChoice);
+    const computerRoll = Math.floor(Math.random()*3); //start by using some random function
+    switch(computerRoll){
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors" ;
+            break;
+    }
+    console.log("Computer chose: " + computerChoice);
 }
 
+function getHumanChoice() {
+    const humanChoice = prompt("Please choose Rock (r), Paper (p) or Scissors (s)");
+    
+    if(humanChoice === "r"){
+        console.log("You chose: Rock");
+    }
+}
+
+getHumanChoice();
 getComputerChoice();
