2b6745d89fb10abfe6d08d89034814556f942cfa
amorfide <kiasukin@gmail.com>
2024-05-28 21:25:47 +0100

    used math.floor & math.random to get 3 outcomes

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+function getComputerChoice() {
+    //return a random choice of rock, paper scissors
+    const computerChoice = Math.floor(Math.random()*3); //start by using some random function
+    console.log(computerChoice);
+}
+
+getComputerChoice();
